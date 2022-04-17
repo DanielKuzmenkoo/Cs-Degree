c76ea08e0f75d14c742d928425ff58562a797721
Daniel Kuzmenko <kuzmenko.dani@gmail.com>
2022-04-17 21:36:27 +0300

    Bug fixes

--- Temperature_Conversions.py
+++ Temperature_Conversions.py
@@ -1,3 +1,9 @@
-Print("Hello, this is a program that converts Celsius and Fahrenheit Degrees")
-Print("In what units are your Degrees?")
+print("Hello, this is a program that converts Celsius and Fahrenheit Degrees")
+print("In what units are your Degrees?")
+Get_Degrees = input("type in either celsius or Fahrenheit :")
+
+    
+
+
+
 
